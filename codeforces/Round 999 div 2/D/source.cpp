27041ee3bbff372e6e0ea6bd06d19c943fa09367
sp4rkl33 <tuan233790011@gmail.com>
2025-02-22 14:42:26 +0700

    Create source.cpp

---
+++ codeforces/Round 999 div 2/D/source.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+#define retsolve int t; cin>>t; while(t--){cout<<solve()<<'\n';}
+#define voidsolve int t; cin>>t; while(t--){solve();}
+#define ff first
+#define ss second
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class type1>
+using ordered_multiset = tree <type1, null_type, less_equal <type1>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class type1>
+using ordered_set = tree <type1, null_type, less <type1>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}" << "\n";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+
+string solve(){
+   int n, m; cin >> n >> m;
+   map<int, int> mp;
+   for (int i = 0; i < n; i++) {
+       int x; cin >> x;
+       mp[x]++;
+   }
+
+    vector<int> b(m); for (auto &it:b) cin >> it;
+
+   for (int x:b){
+       priority_queue<int, vector<int>, greater<int>> pq;
+       pq.push(x);
+       while (!pq.empty()){
+           int cur = pq.top(); pq.pop();
+           if (cur == 0) return "NO";
+           else if (mp[cur] > 0){
+                mp[cur]--;
+                n--;
+           } else {
+               pq.push(cur / 2);
+               pq.push(cur % 2 + cur / 2);
+           }
+       }
+   }
+
+   return n > 0 ? "NO" : "YES";
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	retsolve
+}
